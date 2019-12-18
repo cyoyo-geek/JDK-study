59b46ff0f4e06ba7557c4c0980ee64562e6f0eb6
hexiaobing5880 <823036978@qq.com>
2019-12-18 08:53:57 +0800

    'jdk1.8.0_131源码学习'

---
+++ jdk源码.txt
@@ -0,0 +1,23 @@
+基本类型的包装类(Character放在最后)
+String、StringBuffer、StringBuilder、StringJoiner、StringTokenizer(补充正则表达式的知识)
+CharacterIterator、StringCharacterIterator、CharsetProvider、CharsetEncoder、CharsetDecoder(较难)
+java.util.function下的函数表达式
+java.nio下的各种Buffer实现
+java.lang.ref和jdk.internal.ref下的各种引用：软引用/弱引用/虚引用
+Unsafe的实现(JDK9之后有两个同名类，一个引用了另一个，建议放在一起阅读)
+java.util.stream下的流式编程的实现(很难)
+Thread和ThreadLocal
+Math、Random、BigInteger、BigDecimal
+java.lang.reflect下反射的实现(先掌握JDK 9之后引入的模块系统)
+ClassLoader的实现
+javax.lang.model下Java语言模型的实现(可以参考Java官方语法文档)
+注解(需要彻底掌握)
+Timer、ResourceBundle、Properties
+时间日期类型(尤其是Java8新增的部分)
+java.lang.reflect.Proxy， JDK默认的动态代理
+java.util.concurrent并发包。先读原子类，再读锁的实现类，最后阅读那些并发工具的实现(很难)
+集合框架，主要是三大类：List、Set、Map(先读非线程安全的实现，再读线程安全的实现)
+网络编程(主要阅读Socket通信部分，后续可以阅读HttpClient的实现)
+IO/NIO/BIO(很难)
+Files、Path等文件操作工具类
+sql、xml处理类/接口
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] a = {810,990,1440,990,1350,1800,990,1350,1800,810,1170,1620,720,900,1080,
+                1350,720,900,1080,1350,720,900,1080,1350,765,1080,1260,900,1170,1440,1980,
+                945,1170,1440,1620,720,900,1170,1440,720,990,1260,720,990,1260,720,990,1260,
+                720,900,1080,1440,900,120,1620,720,900,1080,1440,720,900,1080,1440,720,900,
+                1080,1440,900,1080,1440,1080,1440,1620,990,1350,1530,810,990,1260,1620};
+//        int[] b= {1,2};
+//        System.out.println(twoSum(a, 2600));
+//        System.out.println(Arrays.toString(twoSum(b, 3)));
+
+    }
+}
