33f12b24f5a6fe5d560f5181457e90e9d07912b4
hexiaobing5880 <823036978@qq.com>
2019-12-27 09:34:46 +0800

    并发编程的艺术-chapter04

---
+++ src/test/ArtConcurrentBook/chapter04/SleepUtils.java
@@ -0,0 +1,15 @@
+package test.ArtConcurrentBook.chapter04;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 6-4
+ */
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
